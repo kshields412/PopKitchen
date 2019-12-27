2e86f08ad1c25b8125022a74f76ecd66325e104c
kshields412 <kshields412@gmail.com>
2019-12-27 17:57:07 -0500

    home page and header updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {SlideshowModule} from 'ng-simple-slideshow';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlideshowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/images.ts
@@ -0,0 +1,14 @@
+export const images: string[] = [
+    '../../assets/chicken-buns.jpg',
+    '../../assets/chocolate-cake-dessert.jpg',
+    '../../assets/food-truck-burgers.jpg',
+    '../../assets/food-truck-tacos-and-burgers.jpg',
+    '../../assets/food-truck-tacos.jpg',
+    '../../assets/french-toast.jpg',
+    '../../assets/jalapeno-lime-soup-1.jpg',
+    '../../assets/orange-berry-cocktail.jpg',
+    '../../assets/rice-fish-egg.jpg',
+    '../../assets/seafood-spread.jpg',
+    '../../assets/sushi-spread.jpg', 
+    '../../assets/about-titan-craft-cocktails-7.jpg'
+];
