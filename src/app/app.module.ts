ac8b390c0c20a6c195b7bdf471793831485a4708
kshields412 <kshields412@gmail.com>
2019-12-30 20:21:11 -0500

    working on login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import {SlideshowModule} from 'ng-simple-slideshow';
+import { SlideshowModule } from 'ng-simple-slideshow';
 import { LoginComponent } from './login/login.component';
 import { SpotlightComponent } from './spotlight/spotlight.component';
 import { NewThisWeekComponent } from './new-this-week/new-this-week.component';
 import { SearchComponent } from './search/search.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DomService } from './dom.service';
+import { ModalService } from './modal.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,13 @@ import { ProfileComponent } from './profile/profile.component';
     AppRoutingModule,
     SlideshowModule
   ],
-  providers: [],
+  providers: [
+    DomService,
+    ModalService
+  ],
+  entryComponents:[
+    LoginComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '.././modal.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit() {
   }
 
+  initLoginModal() {
+    let inputs = {
+      isMobile: false
+    }
+    this.modalService.init(LoginComponent, inputs, {});
+  }
+  
 }
