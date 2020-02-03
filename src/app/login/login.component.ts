4fa447ce8793691edcc6451a7c84eb6979151ff2
Kai Shields <kaishields@Kais-MacBook-Air.local>
2020-02-03 18:37:45 -0500

    fixing login modal

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalService } from '../modal.service';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  closeResult: string;
 
-  constructor(private modalService: ModalService) { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit() {
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
 
-  public close() {
-    this.modalService.destroy();
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
-
 }
